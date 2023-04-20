702b9acc56dd0571915f3ee871651cb1f8cc7cb7
Satyam Singh <singh.k.satyam1998@gmail.com>
2023-04-20 14:53:26 +0530

    added genenrate para

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const input = document.getElementById("numOfWords");
+const container = document.querySelector('.container');
 
-const getData = () => {
-    console.log(input.value);
-}
+const generateWord = (n) => {
+    const letter = 'QWERTYUIOPLKJHGFDSAZXCVBNM';
+    let word = '';
+
+    for (let index = 0; index < n; index++) {
+        word += letter[(Math.random() * (letter.length - 1)).toFixed(0)];  
+    };
+    return word;
+};
+
+let numOfWords;
+const generatePara = () => {
+    numOfWords = input.value;
+
+    const para = document.createElement('p');
+
+    para.setAttribute('class','paras');
+    
+    para.innerText = 'sample para';
+
+    container.append(para);
+
+};
