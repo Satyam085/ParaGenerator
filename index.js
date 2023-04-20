50541cc53b73e4236195c6790f2c6ed79aad3b6a
Satyam Singh <singh.k.satyam1998@gmail.com>
2023-04-20 15:20:07 +0530

    finished project

--- index.js
+++ index.js
@@ -18,9 +18,27 @@ const generatePara = () => {
     const para = document.createElement('p');
 
     para.setAttribute('class','paras');
+
+    let data = '';
+    for (let index = 0; index < numOfWords; index++) {
+        data += generateWord((Math.random() * 15).toFixed(0));
+        data = toTitleCase(data);
+        data += ' ';
+    }
     
-    para.innerText = 'sample para';
+    para.innerText = data;
 
     container.append(para);
 
 };
+
+function toTitleCase(str) {
+    return str.replace(
+      /\w\S*/g,
+      function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      }
+    );
+  }
+
+
